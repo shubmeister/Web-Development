8e4ab6a9dcbeee232efec2099f27ec065adc27c0
Shubham Negi <negishub888@gmail.com>
2023-06-01 22:45:19 +0530

    tagname
    web dev tagName

---
+++ assign3.js
@@ -0,0 +1,10 @@
+
+var item=document.getElementsByClassName('list-item');
+
+console.log(item);
+
+item[0].textContent='Hello world';
+
+var tag=document.getElementsByTagName('li');
+
+tag[4].style.color='red';
